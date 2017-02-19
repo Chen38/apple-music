1524570aeb88e555efa30a6fd5541867cb3afd5e
Barry Chen <spchen38@163.com>
2017-02-19 20:48:07 +0800

    first commit, a lots of work to do

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # apple-music
-仿写 Apple Music 应用界面，锻炼加强自己的 CSS 能力
+
+闲来无聊，发现 `Apple Music` 的界面特别好看，于是决定练练自己的动手能力，利用 `HTML5` 和 `CSS3` 来仿写一个。
+
+### 我仿写的界面效果图
+
+> 这是播放时的界面：
+
+![Apple Music play](assets/hc-play.png)
+
+> 这是暂停时的界面：
+
+![Apple Music Pause](assets/hc-pause.png)
+
+看上去还是有一点点不同的，后期还要进行优化，增加更多的页面，尽量接近原生吧。
+
+### 剩下的工作
+
+* 右边的音量还有 *3* 个波纹
+
+![Volume](assets/volume.jpg)
+
+* 最底下的三角形是外放设备选择，同样也有三个波纹
+
+![Speaker](assets/speaker.jpg)
+
+### 总结
+
+* 样式表是用 `SASS` 配合 `Compass` 来书写的，嵌套书写和函数的复用，整个结构比较清楚，也可以减少书写的代码量。
+
+* 导航条和专辑都是用的图片，其他都是用 `CSS` 书写。
+
+* 还增加了一点效果，反复点击播放按键时专辑图片的动效，主要是 `className` 的增加和移除。
+
+* 能用伪类的尽量用伪类，减少 `HTML` 的书写量。
---
+++ js/app.js
@@ -0,0 +1,164 @@
+!function(root) {
+	
+	$.computedMosuePos = function(cx, cy) {
+		
+	}
+	
+	root.requestAnimationFrame = root.requestAnimationFrame || root.mozRequestAnimationFrame || root.webkitRequestAnimationFrame || root.msRequestAnimationFrame || function(callback) {
+		return root.setTimeout(callback, 1000/60);
+	}
+	
+	// album toggle scale
+	var $pp = $('.pp'),
+		$album = $('.album'),
+		music = $('.music')[0];
+	
+	var needClass = ['album-big', 'pp-play'];
+	
+	function toggleAlbumPlay() {
+		if(!$album.hasClass(needClass[0])) {
+			$album.addClass(needClass[0]);
+			$pp.addClass(needClass[1]);
+			// music.play();
+		} else {
+			$album.removeClass(needClass[0]);
+			$pp.removeClass(needClass[1]);
+			// music.pause();
+		}
+	}
+	
+	// text slide
+	var PADDING_LEFT = 64,
+		slideStep = 0;
+	var desc = $('.desc')[0],
+		descText = desc.innerText,
+		offsetWidth = desc.offsetWidth,
+		scrollWidth = desc.scrollWidth;
+	
+	// (function slide() {
+		
+	// 	window.requestAnimationFrame(slide);
+	// 	slideStep--;
+		
+	// 	if (-slideStep > scrollWidth - offsetWidth + PADDING_LEFT) {
+	// 		desc.innerHTML = descText + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + descText;
+	// 		offsetWidth = desc.offsetWidth;
+	// 		scrollWidth = desc.scrollWidth;
+	// 		slideStep = 0;
+	// 	}
+		
+	// 	desc.style.transform =
+	// 	desc.style.webkitTransform = 'translate3d(' + slideStep + 'px, 0, 0)';
+		
+	// }());
+	
+	// btn bg show hide
+	var $upBtns = $('.up span'),
+		$downBgs = $('.down span');
+	
+	$upBtns.on('touchend', function() {
+		
+		var index = $(this).index();
+		if (index === 1) {
+			toggleAlbumPlay();
+		}
+		
+		$downBgs.eq(index).addClass('bg-show-hide');
+		
+		$(this).addClass('down-up');
+		
+	});
+	
+	$downBgs.on('animationend webkitAnimationend', function() {
+		$(this).removeClass('bg-show-hide');
+	});
+	
+	$upBtns.on('animationend webkitAnimationend', function() {
+		$(this).removeClass('down-up');
+	});
+	
+	// timeline control
+	var $indicator = $('.indicator'),
+		$played = $('.played'),
+		$line = $('.line'),
+		$playTime = $('.play'),
+		$restTime = $('.rest');
+	var isDown = false,
+		lineOL = $line.offset().left;
+	
+	$indicator.on('touchstart', function(event) {
+		isDown = true;
+		$indicator.addClass('indicator-big');
+		$playTime.addClass('play-down');
+		if (!$album.hasClass(needClass[0])) {
+			$album.addClass('album-pause-up');
+		} else {
+			$album.addClass('album-play-up');
+		}
+	});
+	
+	$indicator.on('touchmove', function(event) {
+		
+	});
+	
+	$indicator.on('touchend', function(event) {
+		isDown = false;
+		$indicator.removeClass('indicator-big');
+		$playTime.removeClass('play-down');
+		if (!$album.hasClass(needClass[0])) {
+			$album.removeClass('album-pause-up');
+		} else {
+			$album.removeClass('album-play-up');
+		}
+	});
+	
+	// volume control
+	var $progress = $('.progress'),
+		$v_indicator = $('.v-indicator'),
+		$volumes = $('.volumes');
+	
+	var vOL = $volumes.offset().left,
+		viOL = $v_indicator.offset().left,
+		wLA = $volumes.width(),
+		wV = $v_indicator.width(),
+		wP = $progress.width(),
+		OML, recordOL = 0, oLeft;
+	
+	$v_indicator.on('touchstart', function(event) {
+		isDown = true;
+		var cx = event.touches[0].pageX;
+		if (cx <= wV/2 + vOL) {
+			cx = wV/2 + vOL;
+			viOL = vOL;
+		}
+		OML = cx - viOL;
+	});
+	
+	$v_indicator.on('touchmove', function(event) {
+		if (isDown) {
+			var cx = event.touches[0].pageX;
+			if (cx <= wV/2 + vOL) {
+				cx = wV/2 + vOL;
+			}
+			if (cx >= wLA - wV/2 + vOL) {
+				cx = wLA - wV/2 + vOL;
+			}
+			oLeft = cx - viOL - OML + recordOL;
+			$(this).css({
+				'transform': 'translate3d(' + oLeft + 'px, 0, 0)',
+				'webkitTransform': 'translate3d(' + oLeft + 'px, 0, 0)'
+			});
+			$progress.css({
+				'width': wP + oLeft + 'px'
+			});
+		}
+	});
+	
+	$v_indicator.on('touchend', function(event) {
+		isDown = false;
+		viOL = $(this).offset().left;
+		recordOL = oLeft;
+	});
+	
+	
+}(this);
